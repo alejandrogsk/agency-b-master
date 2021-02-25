a70e4e3a46fb8c525889d059b27012fdf0a56509
alejandro <alejandrogsk9900@gmail.com>
2021-02-25 10:00:44 -0300

    javascript add

--- javascript.js
+++ javascript.js
@@ -1,11 +1,29 @@
 //Navbar fixed
-const position = window.addEventListener('scroll', () => {
+document.addEventListener("DOMContentLoaded", () => {
+    headerHideShow();
+    closeMenu();
+})
 
+
+const headerHideShow = () => {
     const $header = document.getElementById('header-f');
 
-    if ( window.scrollY > 300 && window.scrollY < 3700 ) {
-        $header.classList.add('header-fix')
-    } else {
-        $header.classList.remove('header-fix')
-    }
-});
+    window.addEventListener('scroll', () => {
+
+        if ( window.scrollY > 300 && window.scrollY < 3700 ) {
+            $header.classList.add('header-fix')
+        } else {
+            $header.classList.remove('header-fix')
+        }
+
+    });
+
+}
+
+const closeMenu = () => {
+    let $links = document.querySelectorAll("a.navbar-collapse")
+
+    $links.addEventListener("click", (e) => {
+        return console.log("function closeMenu", e)
+    })
+}
