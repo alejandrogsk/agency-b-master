4b6a2cb5ce908fafda71f4f062c145913c0e1359
alejandro <alejandrogsk9900@gmail.com>
2021-02-25 22:48:59 -0300

    js modifications

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     headerHideShow();
+    console.log(screen.width)
 })
 
 
@@ -8,7 +9,7 @@ const headerHideShow = () => {
 
     window.addEventListener('scroll', () => {
 
-        if ( window.scrollY > 300 && window.scrollY < 3700 ) {
+        if ( window.scrollY > 300 && window.scrollY < 3700 && screen.width > 1000) {
             $header.classList.add('header-fix')
         } else {
             $header.classList.remove('header-fix')
